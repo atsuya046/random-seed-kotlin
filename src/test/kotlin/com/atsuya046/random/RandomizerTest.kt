f2a1bea18a7c603c7288e48cfdd03e075b111d97
Nobuya  Oshiro <nobuya.oshiro@gmail.com>
2018-03-03 01:18:46 +0900

    format

--- src/test/kotlin/com/atsuya046/random/RandomizerTest.kt
+++ src/test/kotlin/com/atsuya046/random/RandomizerTest.kt
@@ -16,8 +16,8 @@ internal class RandomizerTest {
     inner class DoubleRandomizerTest : AbstractRandomizerTest<Double>(DoubleRandomizer)
 
     @Nested
-    inner class StringRandomizerTest: AbstractRandomizerTest<String>(StringRandomizer)
+    inner class StringRandomizerTest : AbstractRandomizerTest<String>(StringRandomizer)
 
     @Nested
-    inner class CharArrayRandomizerTest: AbstractRandomizerTest<CharArray>(CharArrayRandomizer)
+    inner class CharArrayRandomizerTest : AbstractRandomizerTest<CharArray>(CharArrayRandomizer)
 }
