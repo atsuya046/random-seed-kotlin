3544edb7eacfd2b6b65bfdd6ad08fef8b0f5b705
Nobuya  Oshiro <nobuya.oshiro@gmail.com>
2018-03-08 01:06:10 +0900

    refactor unit test

--- src/test/kotlin/com/atsuya046/random/RandomTest.kt
+++ src/test/kotlin/com/atsuya046/random/RandomTest.kt
@@ -5,8 +5,14 @@ import org.junit.jupiter.api.Test
 
 internal class RandomTest {
     @Test
+    @Suppress("USELESS_IS_CHECK")
     fun generate() {
-        val randomValue = Random().generate<Int>()
-        assertTrue { Int.MIN_VALUE <= randomValue && randomValue <= Int.MAX_VALUE }
+        val random = Random()
+        assertTrue(random.generate<String>() is String)
+        assertTrue(random.generate<CharArray>() is CharArray)
+        assertTrue(random.generate<Int>() is Int)
+        assertTrue(random.generate<Long>() is Long)
+        assertTrue(random.generate<Float>() is Float)
+        assertTrue(random.generate<Double>() is Double)
     }
 }
